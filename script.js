bf1f58eed630890d9af7e24a5e2d7974dcf8bc2c
Gagan813 <gagan.b.l.gowda@gmail.com>
2024-12-17 01:01:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function createAccount() {
+    alert('Login successful!');
+    window.location.href = "dashboard.html";
+}
+
+function redirectToDashboard() {
+    alert('Account created successfully!');
+    window.location.href = "dashboard.html";
+}
