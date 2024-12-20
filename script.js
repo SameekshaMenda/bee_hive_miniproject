ee41b8e324e2bd9917c175a82763695ba084e87d
SameekshaMenda <sameekshamenda19@gmail.com>
2024-12-20 16:35:13 +0530

    changged

--- script.js
+++
@@ -1,9 +0,0 @@
-function createAccount() {
-    alert('Login successful!');
-    window.location.href = "dashboard.html";
-}
-
-function redirectToDashboard() {
-    alert('Account created successfully!');
-    window.location.href = "dashboard.html";
-}
